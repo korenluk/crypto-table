d60b344a9376bb09eba42e73f5058e1e2c3e1769
Lukas Korinek <lukas.korinek@cleverlance.com>
2021-09-08 15:00:53 +0200

    add cache to api manager

--- crypto-table/Networking/CryptoAPIManager.swift
+++ crypto-table/Networking/CryptoAPIManager.swift
@@ -8,45 +8,67 @@
 import Foundation
 
 final class CryptoAPIManager {
-    let jsonDecoder = JSONDecoder()
+    private let jsonDecoder = JSONDecoder()
+
+    private lazy var session: URLSession = {
+        let sessionConfiguration = URLSessionConfiguration.default
+        sessionConfiguration.requestCachePolicy = .returnCacheDataElseLoad
+        return URLSession(configuration: sessionConfiguration)
+    }()
+
+    private func decode<T: Decodable>(data: Data, completion: @escaping (Swift.Result<T, Error>) -> Void) {
+        do {
+            let cryptoResponse = try self.jsonDecoder.decode(CryptoResponse.self, from: data)
+            if cryptoResponse.success == true {
+                let object = try self.jsonDecoder.decode(T.self, from: data)
+                DispatchQueue.main.async {
+                    completion(.success(object))
+                }
+            } else {
+                let cryptoError = try self.jsonDecoder.decode(CryptoError.self, from: data)
+                completion(.failure(cryptoError))
+            }
+        } catch {
+            completion(.failure(error))
+        }
+    }
 }
 
 extension CryptoAPIManager: APIManaging {
     func request<T: Decodable>(request: URLRequest, completion: @escaping (Swift.Result<T, Error>) -> Void) {
-        let urlSession = URLSession.shared.dataTask(with: request) { data, response, error in
 
-            if let httpResponse = response as? HTTPURLResponse,
-               !(200..<300).contains(httpResponse.statusCode) {
-                completion(.failure(CryptoError(success: false,
-                                                error: CryptoDetailError(code: httpResponse.statusCode,
-                                                                         type: "Bad Response",
-                                                                         info: httpResponse.description))))
-                return
-            }
+        if let cachedData = URLCache.shared.cachedResponse(for: request) {
+            print("Cached data in bytes:", cachedData.data)
 
-            guard let data = data else {
-                completion(.failure(CryptoError(success: false,
-                                                error: CryptoDetailError(code: 0,
-                                                                         type: "Data",
-                                                                         info: "Data corupted"))))
-                return
-            }
+            decode(data: cachedData.data, completion: completion)
 
-            do {
-                let object = try self.jsonDecoder.decode(CryptoResponse.self, from: data)
-                if object.success == true {
-                    let cryptoLive = try self.jsonDecoder.decode(T.self, from: data)
-                    DispatchQueue.main.async {
-                        completion(.success(cryptoLive))
-                    }
-                } else {
-                    let cryptoError = try self.jsonDecoder.decode(CryptoError.self, from: data)
-                    completion(.failure(cryptoError))
+        } else {
+            session.dataTask(with: request) { data, response, error in
+
+                if let httpResponse = response as? HTTPURLResponse,
+                   !(200..<300).contains(httpResponse.statusCode) {
+                    completion(.failure(CryptoError(success: false,
+                                                    error: CryptoDetailError(code: httpResponse.statusCode,
+                                                                             type: "Bad Response",
+                                                                             info: httpResponse.description))))
+                    return
                 }
-            } catch {
-                completion(.failure(error))
-            }
+
+                guard let data = data, let response = response else {
+                    completion(.failure(CryptoError(success: false,
+                                                    error: CryptoDetailError(code: 0,
+                                                                             type: "Data",
+                                                                             info: "Data corupted"))))
+                    return
+                }
+
+                let cachedData = CachedURLResponse(response: response, data: data)
+                URLCache.shared.storeCachedResponse(cachedData, for: request)
+
+                self.decode(data: data, completion: completion)
+
+            }.resume()
+
         }
-        urlSession.resume()
     }
 }
